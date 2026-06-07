0dfc71fc1bc7ef71a5e3ac1d4854b3423bf34686
lucifron28 <lucifron28@gmail.com>
2026-06-07 14:21:38 +0800

    chore(release): bump version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.example.sona"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
